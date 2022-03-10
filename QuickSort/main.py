fc0a2a376e511d96f4d544cecba692f702807856
Galal Mohammed <galalmoh2001@gmail.com>
2022-03-10 15:15:24 +0200

    Uploading Quick Sort implementation

---
+++ QuickSort/main.py
@@ -0,0 +1,9 @@
+def QuickSort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return QuickSort(less) + [pivot] + QuickSort(greater)
+print(QuickSort([10, 5, 2, 3]))
